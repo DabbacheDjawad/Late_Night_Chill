7c3be3dab17650b022bc5ad24214f91610010ac7
Djawad Dabbache <djawadmne@gmail.com>
2025-11-17 20:00:35 +0100

    minor problem

--- app/(root)/anime/[id]/characters/page.tsx
+++ app/(root)/anime/[id]/characters/page.tsx
@@ -1,5 +1,6 @@
 import Characters from "@/Components/Characters";
 
-export default function Page({ params }: { params: { id: string } }) {
-  return <Characters id={params.id} />;
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+  const {id} = await params;
+  return <Characters id={id} />;
 }
