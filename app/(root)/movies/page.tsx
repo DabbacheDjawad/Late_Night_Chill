2d84cabdb62fe0d5e6194dc78d7dc0f346cfbd7d
Djawad Dabbache <djawadmne@gmail.com>
2025-11-21 16:46:43 +0100

    minor problem

--- app/(root)/movies/page.tsx
+++ app/(root)/movies/page.tsx
@@ -1,10 +1,4 @@
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
-export const runtime = "nodejs";
-export const revalidate = 0;
-  
-
-
+"use client";
 import AnimeGridSkeleton from "@/Components/AnimeGridSkeleton";
 import SearchForm from "@/Components/SearchForm";
 import ShowsGrid from "@/Components/ShowsGrid";
